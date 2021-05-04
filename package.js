398057f14e5cdbc0794f53af6e5d99a729511cc9
Igor Fontana <igor193@gmail.com>
2021-05-04 04:11:38 -0300

    ICANN Authorization

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "ICANN OAuth flow",
-  version: "1.0.2",
+  version: "1.0.3",
   name: "pathable:icann-oauth",
   git: "https://github.com/pathable/icann",
 });
@@ -18,5 +18,5 @@ Package.onUse((api) => {
   api.addFiles("icann_client.js", "client");
   api.addFiles("icann_server.js", "server");
 
-  api.export("icann");
+  api.export("Icann");
 });
