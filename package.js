4ba18d88373d3244701682383d73401547d06ee1
Igor Fontana <igor193@gmail.com>
2021-08-05 08:03:23 -0300

    TP-15511 Upgrade to Meteor 2.3 - upgrade oauth

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   summary: "ICANN OAuth flow",
-  version: "1.0.11",
+  version: "1.1.0",
   name: "pathable:icann-oauth",
   git: "https://github.com/pathable/icann",
 });
 
 Package.onUse((api) => {
-  api.versionsFrom("1.10.3-beta.9");
+  api.versionsFrom('2.3.4');
 
   api.use("ecmascript", ["client", "server"]);
   api.use("oauth2", ["client", "server"]);
